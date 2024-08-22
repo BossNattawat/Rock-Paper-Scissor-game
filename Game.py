5c73c79fd511232fbc4cda5283ae6bd0359c6fb1
boss <75540581+Boss22341231@users.noreply.github.com>
2024-08-22 15:19:58 +0700

    first commit

---
+++ Game.py
@@ -0,0 +1,52 @@
+import random
+
+def main():
+    
+    while True:
+        print("_________ Rock Paper Scissor game _________")
+        print()
+        
+        choice = ["Rock", "Paper", "Scissors"]
+        
+        while True:
+            try:
+                user_input = input("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors\n\nAnswer : ")
+                
+                if user_input not in choice:
+                    print("The answer must be Rock, Paper, or Scissors.")
+                    pass
+                else:
+                    break
+            except ValueError:
+                pass
+            
+        computer_answer = random.choice(choice)
+        print(f"Computer Answer : {computer_answer}")
+        print()
+        
+        if user_input == "Rock" and computer_answer == "Scissors":
+            print("\tYou Win!")
+        elif user_input == "Paper" and computer_answer == "Rock":
+            print("\tYou Win!")
+        elif user_input == "Scissors" and computer_answer == "Paper":
+            print("\tYou Win!")
+        elif user_input == "Rock" and computer_answer == "Paper":
+            print("\tComputer Win!")
+        elif user_input == "Paper" and computer_answer == "Scissors":
+            print("\tComputer Win!")
+        elif user_input == "Scissors" and computer_answer == "Rock":
+            print("\tComputer Win!")
+        else:
+            print("\tTie!")
+        
+        print()
+        
+        again = input("Want to play more? (y/n) : ")
+        
+        if again == "n":
+            break
+        else:
+            pass
+    
+if __name__ == "__main__":
+    main()
